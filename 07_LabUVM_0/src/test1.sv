ceccab294a3c1a8398cb5ec0310de586c69f3361
Guilherme Lira Ribeiro <gui.liraribeiro10@gmail.com>
2026-06-16 20:17:21 -0300

    07 lab 0 de verificação CI Expert (test4/7)

--- 07_LabUVM_0/src/test1.sv
+++ 07_LabUVM_0/src/test1.sv
@@ -13,25 +13,36 @@ typedef logic [3:0] addr_t;
 class MemTrans;
   byte_t data_in;
   addr_t address;
+  byte_t id;
+
+  static byte_t n_instances = 0; // obs: dentro de classes tudo é
+                                 // automético por padrão
 
   // test2 - custon contructor
   function new (addr_t addr = 0, byte_t data = 0);
   begin 
+    this.id = n_instances++; 
     this.data_in = data;
     this.address = addr;
   end
   endfunction
 
   function print();
-    $display("[MemTrans] data_in: %h, address: %h", data_in, address);
+    $display("[MemTrans %d] data_in: %h, address: %h", id, data_in, address);
   endfunction
 endclass
 
-module test1;
+module test;
   initial begin
     MemTrans memtr1 = new(.addr(2));
     MemTrans memtr2 = new(3, 4);
     memtr1.print();
     memtr2.print();
+
+    memtr1.address = 4'hf;
+    memtr1.print();
+    memtr2.print();
+    
+    memtr2 = null;
   end 
 endmodule
